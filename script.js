ece2cfd56aaf5ad66dbc57b1171c14f57e840c6d
Kalso <kalsofun@gmail.com>
2026-09-14 13:04:17 +0800

    added button to pause or play bgm

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ function playAudio(src) {
     let audio = new Audio(src);
     audio.play();
 }
+
+const audio = document.getElementById('bgm');
+const btn = document.getElementById('bgm-control');
+btn.addEventListener('click', () => {
+    if (audio.paused) {
+        audio.play();
+        btn.innerHTML = '❚❚';
+    } else {
+        audio.pause();
+        btn.innerHTML = '▶︎';
+    }
+});
